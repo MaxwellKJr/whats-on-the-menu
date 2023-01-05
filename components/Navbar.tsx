cc11fd84f9c8d7a84e462f703cd248f2dab304ea
Maxwell Kapezi, Jr <maxwellkjr@gmail.com>
2023-01-05 03:02:36 +0200

    Basic Navbar and fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
+import React, {FC} from 'react'
 import Link from 'next/link'
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => {
+const Navbar: FC = () => {
   return (
   <nav className='bg-red-200 w-full h-16'>
       <div className='flex lg:flex-row justify-between items-center mx-auto w-4/5 h-full'>
@@ -9,11 +11,17 @@ const Navbar = () => {
           What's On The Menu?
         </Link>
 
-        <ul>
+        <ul className='flex flex-col sm:flex-row justify-evenly items-center'>
           <li>
             <Link href='/'>Home</Link>
           </li>
+          <li>
+            <Link href='/restaurants'>Restaurants</Link>
+          </li>
         </ul>
+        <div className='block sm:hidden'>
+          <FontAwesomeIcon icon={faBars} />  
+        </div>
       </div>
   </nav>
   )
